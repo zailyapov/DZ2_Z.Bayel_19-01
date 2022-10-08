1934b2202221a4851c6fed6ff613e5283ed998ee
zailyapov <115218244+zailyapov@users.noreply.github.com>
2022-10-08 17:58:07 +0600

    Add files via upload

---
+++ DZ2_Z.Bayel_19-01/app.js
@@ -0,0 +1,33 @@
+
+
+
+// let user = new Object(123);
+// console.log (user)
+
+// //      ОБЬЕКТЫ     //
+// let user = {
+//     name: 'Bob',
+//     age: '20',
+//     adress: 'Bishkek',
+// }
+//     console.log(user)
+
+// for (let a=0; a<3; i++) {
+//     alert(a)
+// }
+
+//      ЦИКЛЫ       //
+let i = 1;
+while (i < 51   ) {
+    console.log(i);
+    i++;
+}
+
+//       МАСИВЫ     //
+let fruits = ['Привет', 'Мир', '!']
+    alert(fruits[0])
+    alert(fruits[1])
+    alert(fruits[2])
+console.log(fruits);
+    
+
